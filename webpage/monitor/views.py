ad89483e29fde45ac8ba769440cf3e38656b457e
Sheng Wu <pkernoobie@gmail.com>
2013-04-30 11:09:01 -0500

    Randomize cries for testing

--- webpage/monitor/views.py
+++ webpage/monitor/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render 
 from django_socketio import broadcast, NoSocket
 from monitor.models import *
+import random
 
 def home(request):
     try:
@@ -21,10 +22,11 @@ def alert(request):
     # Since they're based on claps, use all same fields for now
     # In the future, maybe send length/volume parameters in the URL
     cry = Cry()
-    cry.length = 0.1 # in seconds
-    cry.volume = 100 # in decibels
+    cry.length = random.uniform(0.1, 3.0) # in seconds
+    cry.volume = random.uniform(60, 110) # in decibels
     cry.save()
-    print "Cry recorded at %s" % cry.time
+    print "Cry recorded at %s\nLength: %f\nVolume: %f" % \
+        (cry.time, cry.length, cry.volume)
     return HttpResponse("We get it. you're crying. wa wa waaa")
 
 def cries(request):
