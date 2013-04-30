69d50ff43a855104cafaee67e30e52c20a83606f
Alex Jablonski <alexmjablonski@gmail.com>
2013-04-30 17:52:08 +0100

    Fix simple data read from temperature sensor

--- temperature/temp.c
+++ temperature/temp.c
@@ -4,50 +4,54 @@
  */
 #include <assert.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <wiringPi.h>
+
 
 void startSignal(int pin)
 {
     pinMode(pin, OUTPUT);        
     digitalWrite(pin, LOW);
-    delay(10); // 10 ms
+    delayMicroseconds(1100); // 1.1 ms
     digitalWrite(pin, HIGH);
-    delayMicroseconds(30); // range is 20-40 us
+    delayMicroseconds(35); // range is 20-40 us
+}
 
+void readData(int pin, char *data_ptr)
+{
     pinMode(pin, INPUT);
-
     int limit = 0;
-    
-    // some waiting for RHT03 to pull line low (possibly unnecessary)
-    while(digitalRead(pin) == HIGH) {
+    while (limit < 10000) {
+        data_ptr[limit] = digitalRead(pin);
         delayMicroseconds(2);
-        printf("loop 1");
-    }
-    while(digitalRead(pin) == LOW) {
-        delayMicroseconds(2);
-        printf("loop 2");
-    }
-    // currently, we never get here :(
-    while(digitalRead(pin) == HIGH) {
-        delayMicroseconds(2);
-        printf("loop 3");
-    }
-
-    // test function 
-    while(limit < 1000) {
-        printf("%d", digitalRead(pin));
         limit++;
     }
 }
+
+void dispData(char *data_ptr)
+{
+    int limit = 0;
+    while (limit < 10000) {
+        printf("%d", data_ptr[limit]);
+        limit++;
+    }
+}
+
 
 int main (void)
 {
     printf ("Raspberry Pi Temp\n") ;
 
+    char *data = malloc(10000);
+
     if (wiringPiSetup () == -1)
         return 1;
 
-    startSignal(3);
+    startSignal(8);
+
+    readData(8, data);
+
+    dispData(data);
 
     return 0;
 }
