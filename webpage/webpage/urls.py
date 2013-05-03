ed7f478eb06a5d435ccb97bfe0c568ae58ed7ca6
Taiyo Sogawa <taiyosogawa@gmail.com>
2013-05-03 13:54:12 -0700

    added registration for owners vs secondary users

--- webpage/webpage/urls.py
+++ webpage/webpage/urls.py
@@ -8,8 +8,8 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'', include('monitor.urls', namespace='monitor')),
 	url(r'^register', 'webpage.views.register', name='register'),
+    url(r'^owner', 'webpage.views.register_owner', name='register_owner'),
 	url(r'^login/', 'webpage.views.login_view', name='login_view'),
-	url(r'^auth/', 'webpage.views.auth', name='auth'),
 	url(r'^logout/', 'webpage.views.logout_view', name='logout_view'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
--- webpage/webpage/views.py
+++ webpage/webpage/views.py
@@ -2,49 +2,63 @@ from django.http import HttpResponse
 from django.shortcuts import render 
 from django.contrib.auth.models import User, Group
 from django.contrib.auth import authenticate, login, logout
+from django.db import IntegrityError
 from monitor.models import *
 
-def login_view(request, message=''):
-	return render(request, 'login.html', {'error_message': message})
+def login_view(request):
+    if(request.user.is_authenticated()):
+		return home(request) 
+    if(not request.POST):
+        return render(request, 'login.html', {'error_message': ''})
+    
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password) 
+    if(user is not None and user.is_active):
+		login(request, user)
+		return home(request)
+    else:
+        return render(request, 'login.html', {'error_message': 'Wrong email or password, you jerk.'})
 
-def register(request, message=''):
-	return render(request, 'register.html', {'error_message': message})
+def register(request):
+    if(not request.POST):
+	    return render(request, 'register.html', {'error_message': ''})
+
+    username = request.POST['username']
+    password = request.POST['password']	
+    if(len(request.POST['name']) < 1):
+        return render(request, 'register.html', {'error_message': 'We need your name, you fool of a Took.'})
+    nameList = request.POST['name'].split(' ',1)
+    firstname = nameList[0]
+    lastname = ""
+    if(len(nameList) > 1):
+        lastname = nameList[1]
+    # process the Password
+    if(password != request.POST['repeat-password']):
+        return render(request, 'register.html', {'error_message': 'Password does not match, you meanie.'})
+    if(len(password) < 4):
+        return render(request, 'register.html', {'error_message': 'Your password needs to be at least 4 characters, you inconsiderate twerp.'})
+    try:
+        user = User.objects.create_user(username=username, password=password, email=username, first_name=firstname, last_name=lastname)
+    except IntegrityError:
+        return render(request, 'register.html', {'error_message': 'That user name already exists, you poopoo head.'})
+    if('owner' not in request.POST):
+        user.is_active = False
+        user.save()
+        return render(request, 'login.html', {'error_message': 'Account created for ' + firstname + '. Have the monitor owner activate your account, you horse thief!'})
+    else:
+        user.save()
+        user = authenticate(username=username, password=password) 
+        login(request, user)
+        return home(request)
 
 def logout_view(request):
-	logout(request)
-	return HttpResponse("You have logged out. Goodbye.")
+    logout(request)
+    return login_view(request)
 
 def home(request):
 	return render(request, 'monitor/home.html', {})
 
-def auth(request):
-	if(request.user.is_authenticated()):
-		return home(request) 
-	elif(not request.POST):
-		return login_view(request, 'Please enter your credentials, you miscreant.')
-	else:
-		username = request.POST['username']
-		password = request.POST['password']
-		
-		# if we are registering a new user
-		if('repeat-password' in request.POST):
-			if(password != request.POST['repeat-password']):
-				return register(request, 'Password does not match, you meanie.')
-			if(len(password) < 4):
-				return register(request, 'Your password needs to be at least 4 characters, you inconsiderate twerp.')
-			try:
-				user = User.objects.create_user(username=username, password=password)
-			except IntegrityError:
-				return register(request, 'That user name already exists, you poopoo head.')
-				user.is_active = False
-				user.save()
-			return login_view(request, 'Have the monitor owner activate your account, you horse thief!')
-							
-		# if we are just logging in
-		else:
-			user = authenticate(username=username, password=password) 
-			if user is not None:
-				if user.is_active:
-					login(request, user)
-					return home(request)
-			return login_view(request, 'Wrong username or password, you jerk.')
+def register_owner(request):
+    logout(request)
+    return render(request, 'register.html', {'owner': 'True'})
