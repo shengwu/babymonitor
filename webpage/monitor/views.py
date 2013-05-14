25a5d7bc4bc8fa35f434ccbed0254bb0a4264835
Sheng Wu <pkernoobie@gmail.com>
2013-05-14 12:57:25 +0100

    Add streaming video to homepage

--- webpage/monitor/views.py
+++ webpage/monitor/views.py
@@ -4,7 +4,9 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django_socketio import broadcast, NoSocket
 from monitor.models import *
+import os
 import random
+import socket
 
 @login_required
 def home(request):
@@ -19,9 +21,29 @@ def home(request):
     with open('/baby/humidity') as f:
         humidity = f.readline().strip()
 
+    # Get current IP
+    if os.name != "nt":
+        import fcntl
+        import struct
+        def get_interface_ip(ifname):
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            return socket.inet_ntoa(fcntl.ioctl(s.fileno(), 0x8915, 
+                       struct.pack('256s', ifname[:15]))[20:24])
+    ip = socket.gethostbyname(socket.gethostname())
+    if ip.startswith("127.") and os.name != "nt":
+        interfaces = ["eth0", "eth1", "eth2", "wlan0", "wlan1", "wifi0", 
+                        "ath0", "ath1", "ppp0"]
+        for ifname in interfaces:
+            try:
+                ip = get_interface_ip(ifname)
+                break
+            except IOError:
+                pass
+
     return render(request, 'monitor/home.html', {
         'temp': temp,
-        'humidity': humidity
+        'humidity': humidity,
+        'ip_address': ip,
         })
 
 def alert(request):
